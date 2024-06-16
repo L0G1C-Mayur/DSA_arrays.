254e96c4c52711fb6938e8d74474835a2fabd764
L0G1C-Mayur <mayurpatil472003@gmail.com>
2024-06-16 21:58:17 +0530

    time complexity - n(logn)

--- Binary_search.java
+++ Binary_search.java
@@ -1,29 +1,31 @@
-public class Binary_search {
-    public static int search (int arr[], int key){
-        int start = 0;
-        int end = arr.length;
-
-        while(start <= end){      // <= imp. --> for the index which becomes start, mid and end.
-            int mid = (start+end)/2;
-
-            if(arr[mid] == key){
-                return mid;
+public class binary_search {
+        public static int search (int arr[], int key){
+            int start = 0;
+            int end = arr.length;
+    
+            while(start <= end){      // <= imp. --> for the index which becomes start, mid and end.
+                int mid = (start+end)/2;
+    
+                if(arr[mid] == key){
+                    return mid;
+                }
+                if(arr[mid]< key){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
             }
-            if(arr[mid]< key){
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
+            return -1;
         }
-        return -1;
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,4,5,6,7};
+            int key = 7;
+    
+            System.out.println("key found at : " + search(arr, key));
+    
+            
+        }
     }
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
-        int key = 7;
+    
 
-        System.out.println("key found at : " + search(arr, key));
-
-        
-    }
-}
